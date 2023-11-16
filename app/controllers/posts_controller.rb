5853302d4bfb218ce998aaa1808a68aea4c847d4
Dorian Oszczęda <vxern@wordcollector.co.uk>
2023-11-16 15:45:13 +0000

    feat: Support creating new blog posts.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,4 +12,19 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
 end
