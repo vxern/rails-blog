bb7504baa8477c7720a21f0245c5f8aa96b0da72
Dorian Oszczęda <vxern@wordcollector.co.uk>
2023-11-16 15:08:38 +0000

    feat: Support viewing singular posts as linked to from the main list.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,10 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
+  def show
+    @post = Post.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    redirect_to root_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get "/posts", to: "posts#index"
+  get "/posts/:id", to: "posts#show", as: :post
+
   root "posts#index"
 end
