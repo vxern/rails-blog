7cf4c6ee6ed293077e1b57d1370f59a65a409996
Dorian Oszczęda <vxern@wordcollector.co.uk>
2023-11-16 16:28:54 +0000

    feat: Support deleting blog posts.

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get "/posts/new", to: "posts#new", as: :new_post
   get "/posts/:id", to: "posts#show", as: :post
   patch "/posts/:id", to: "posts#update", as: :update_post
+  delete "posts/:id", to: "posts#destroy", as: :delete_post
   get "/posts/:id/edit", to: "posts#edit", as: :edit_post
   post "/posts", to: "posts#create", as: :create_post
 end
