9d358b712e7a8c2282c51c4e2be50d3005ccb7ce
Dorian Oszczęda <vxern@wordcollector.co.uk>
2023-11-16 16:19:04 +0000

    feat: Support editing existing blog posts.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -22,6 +22,19 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def post_params
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   get "/posts", to: "posts#index", as: :posts
   get "/posts/new", to: "posts#new", as: :new_post
   get "/posts/:id", to: "posts#show", as: :post
+  patch "/posts/:id", to: "posts#update", as: :update_post
+  get "/posts/:id/edit", to: "posts#edit", as: :edit_post
   post "/posts", to: "posts#create", as: :create_post
 end
